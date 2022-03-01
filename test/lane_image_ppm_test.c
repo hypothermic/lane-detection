504028fd70a37427598fd01798a57748c19c81dd
hypothermic <32428346+hypothermic@users.noreply.github.com>
2022-03-01 11:40:49 +0200

    Fix tests not outputting to the right file

--- test/lane_image_ppm_test.c
+++ test/lane_image_ppm_test.c
@@ -6,14 +6,16 @@
 #include "lane_log.h"
 #include "lane_test_common.h"
 
+// To verify the PPM load/save functionality,
+// this test just loads the image into memory
+// and then saves it to a file
+
 int main(int argc, char **argv) {
 	lane_image_t *image = NULL;
 
 	TEST_CHECK_ARGS(argc, argv);
 
 	TEST_LOAD_IMAGE(argv[1], image);
-
-	output_file = fopen(argv[2], "wb");
 
 	TEST_SAVE_IMAGE(argv[2], image);
 
