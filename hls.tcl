bf07f4fd800ec487c0c7df3fcbc8500b3ccf06de
hypothermic <32428346+hypothermic@users.noreply.github.com>
2022-03-28 17:32:03 +0300

    Fix IP core output location

--- hls.tcl
+++ hls.tcl
@@ -12,7 +12,8 @@
 set TARGET_PART		xc7z020clg400-1
 set TARGET_NAME		lane_vpu
 set TARGET_TOP		vpu_accel_top
-set TARGET_LOCATION	"./build/${TARGET_NAME}.prj/${TARGET_NAME}/impl/ip/com_arobs_lane_detection_vpu_accel_0_1.zip"
+#set TARGET_LOCATION	"./build/${TARGET_NAME}.prj/${TARGET_NAME}/impl/ip/com_arobs_lane_detection_vpu_accel_0_1.zip"
+set TARGET_LOCATION	"./build/${TARGET_NAME}.prj/${TARGET_NAME}/impl/export.zip"
 set TARGET_FILE		[file normalize $TARGET_LOCATION]
 
 # Clock period (should be 10 ns because Arty and PYNQ sys clocks both run at 100MHz)
@@ -85,7 +86,7 @@ if {[info exists ::env(SYNTH)]} {
 
 	csynth_design
 	export_design -format ip_catalog
-	exec mkdir misc
+	exec mkdir -p misc
 	exec cp $TEST_LOGO "./misc/logo.png"
 	exec zip $TARGET_FILE "misc/logo.png"
 	puts "IP Core successfully exported to $TARGET_FILE"
