1372587cca09779b8d397b7cd1ca47393b41cc01
hypothermic <32428346+hypothermic@users.noreply.github.com>
2022-03-31 20:06:03 +0300

    Add commandline tools for synth & impl to bitstream

--- hw.tcl
+++ hw.tcl
@@ -8,9 +8,11 @@
 #
 
 # Which hardware to build
-set TARGET_PART		xc7a35ticsg324-1L
+#set TARGET_PART	xc7a35ticsg324-1L
+set TARGET_PART		xc7z020clg400-1
 set TARGET_NAME		lane_hw
-set TARGET_TOP		top
+#set TARGET_TOP		top
+set TARGET_TOP		bd_wrapper
 
 # Which runs to execute
 set RUN_SYNTH_NAME	synth_1
@@ -20,67 +22,81 @@ set RUN_IMPL_NAME	impl_1
 set ROOT_DIR		.
 set BUILD_DIR		${ROOT_DIR}/build
 set SRC_DIR		${ROOT_DIR}/rtl
+set IP_DIR		[file normalize ${BUILD_DIR}/lane_vpu.prj/lane_vpu/impl/ip]
 
 # Hardware definitions, relative paths to SRC_DIR, could use regex/glob...
 set CONSTRAINTS_FILE	a7-35.xdc
 set SOURCE_FILES	[list top.vhd]
+set IPI_BD_FILE		ibd.tcl
+set IPI_SRC_FILE	[file normalize ${IP_DIR}/com_arobs_lane_detection_vpu_accel_0_1.zip]
 
 # Create the build dir if it doesn't already exist
 file mkdir ${BUILD_DIR}
 
 # Check if all target files exist
 foreach f [list {*}$SOURCE_FILES ${CONSTRAINTS_FILE}] {
-	puts "Verifying existence of file ${SRC_DIR}/${f}"
+	common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Verifying existence of file ${SRC_DIR}/${f}"
 
 	# If a target file doesn't exist, exit with an error message
 	if {[file exists ${SRC_DIR}/${f}] == 0} {
-		puts "File ${SRC_DIR}/${f} does not exist"
+		common::send_gid_msg -ssname BD::TCL -id 2005 -severity "ERROR" "File ${SRC_DIR}/${f} does not exist"
 		exit 2
 	}
 }
 
 # Create or open the project, we don't really care
 if {[file exists ${BUILD_DIR}/${TARGET_NAME}.xpr] == 0} {
-	puts "Creating project ${TARGET_NAME} for part ${TARGET_PART}"
+	common::send_gid_msg -ssname BD::TCL -id 2004 -severity "INFO" "Creating project on disk for ${TARGET_NAME} for part ${TARGET_PART}"
 	create_project ${TARGET_NAME} ${BUILD_DIR} -part ${TARGET_PART}
 
 	# Iterate through the source files and add them one by one
 	foreach f $SOURCE_FILES {
-		puts "Adding source file ${SRC_DIR}/${f} to ${TARGET_NAME}"
-		add_files ${SRC_DIR}/${f}
+		common::send_gid_msg -ssname BD::TCL -id 2003 -severity "INFO" "Adding source file ${SRC_DIR}/${f} to ${TARGET_NAME}"
+		#add_files ${SRC_DIR}/${f}
 	}
 
 	# Add the constraints file to it's separate fileset
-	puts "Adding constraints file ${CONSTRAINTS_FILE} to ${TARGET_NAME}"
-	add_files -fileset constrs_1 ${SRC_DIR}/${CONSTRAINTS_FILE}
+	common::send_gid_msg -ssname BD::TCL -id 2002 -severity "INFO" "Adding constraints file ${CONSTRAINTS_FILE} to ${TARGET_NAME}"
+	#add_files -fileset constrs_1 ${SRC_DIR}/${CONSTRAINTS_FILE}
+
+	# Import the VPU IP core into the IP repositories
+	# For some odd reason we have to update the IP catalog twice, else vivado won't catch it
+	set_property ip_repo_paths "${IP_DIR}" [current_project]
+	update_ip_catalog
+	update_ip_catalog -add_ip "${IPI_SRC_FILE}" -repo_path "${IP_DIR}"
+
+	# Run block design instantiation script
+	common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Instantiating block design from file ${SRC_DIR}/${IPI_BD_FILE}"
+	source "${SRC_DIR}/${IPI_BD_FILE}"
+	make_wrapper -top -files [get_files ${BUILD_DIR}/${TARGET_NAME}.srcs/sources_1/bd/bd/bd.bd]
+	add_files -norecurse ${BUILD_DIR}/${TARGET_NAME}.srcs/sources_1/bd/bd/hdl/bd_wrapper.v
 } else {
 	# Project already exists, open it
-	puts "Opening project ${TARGET_NAME} for part ${TARGET_PART}"
+	common::send_gid_msg -ssname BD::TCL -id 2000 -severity "INFO" "Opening project ${TARGET_NAME} for part ${TARGET_PART}"
 	open_project ${BUILD_DIR}/${TARGET_NAME}.xpr
 }
 
 # Run a quick syntax check before we proceed, this will save a lot of time
-puts "Running preliminary syntax check"
+common::send_gid_msg -ssname BD::TCL -id 2009 -severity "INFO" "Running preliminary syntax check"
 set syntax_errors [check_syntax -return_string]
 if {${syntax_errors} != ""} {
-	puts "Errors in syntax: $syntax_errors"
+	common::send_gid_msg -ssname BD::TCL -id 2008 -severity "ERROR" "Syntax error(s): $syntax_errors"
 	exit 3
 }
 
 # Make sure our top module is indeed the top module
-puts "Setting file order"
-update_compile_order -fileset sources_1
 set_property top ${TARGET_TOP} [current_fileset]
+update_compile_order -fileset sources_1
 
 if {[info exists ::env(SYNTH)]} {
 	# Launch the synthesis run
-	puts "Running synthesis, go grab a coffee."
+	common::send_gid_msg -ssname BD::TCL -id 2006 -severity "INFO" "Running synthesis, go grab a coffee"
 	reset_run ${RUN_SYNTH_NAME}
 	launch_runs ${RUN_SYNTH_NAME}
 	wait_on_run ${RUN_SYNTH_NAME}
 
 	if {[get_property PROGRESS [get_runs ${RUN_SYNTH_NAME}]] != "100%"} {
-		puts "Synth failed with error"
+		common::send_gid_msg -ssname BD::TCL -id 2007 -severity "ERROR" "Synth failed with error"
 		exit 4
 	}
 }
@@ -95,16 +111,19 @@ if {[info exists ::env(TIMING)]} {
 
 if {[info exists ::env(IMPL)]} {
 	# Launch the implementation run and generate the bitstream file
-	puts "Running implementation, go eat lunch."
+	common::send_gid_msg -ssname BD::TCL -id 2004 -severity "INFO" "Running implementation, go eat lunch."
 	reset_run ${RUN_IMPL_NAME}
 	launch_runs ${RUN_IMPL_NAME} -to_step write_bitstream
 	wait_on_run ${RUN_IMPL_NAME}
 
 	# Check the return code of the synth run
 	if {[get_property PROGRESS [get_runs ${RUN_IMPL_NAME}]] != "100%"} {
-		puts "Implementation failed with error"
+		common::send_gid_msg -ssname BD::TCL -id 2005 -severity "ERROR" "Implementation failed with error"
 		exit 5
 	}
+
+	common::send_gid_msg -ssname BD::TCL -id 2002 -severity "INFO" "Generated bitstream: ./build/lane_hw.runs/impl_1/bd_wrapper.bit"
+	common::send_gid_msg -ssname BD::TCL -id 2003 -severity "INFO" "Generated hwhandoff: ./build/lane_hw.gen/sources_1/bd/bd/hw_handoff/bd.hwh"
 }
 
 open_run ${RUN_IMPL_NAME}
@@ -132,6 +151,8 @@ if {[info exists ::env(UPLOAD)]} {
 	program_hw_devices $dev
 
 	refresh_hw_device $dev
+
+	common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Programming done"
 }
 
 exit 0
--- rtl/top.vhd
+++ rtl/top.vhd
@@ -1,15 +1,95 @@
+--!
+--! @file top.vhd
+--! @author Matthijs Bakker
+--! @brief Top-level module for the hardware definition
+--!
+--! This module instantiates the Zynq 7 Processor System
+--! and connects the hardwired IO and memory channels
+--! to the relevant PS IP Core via the block design wrapper
+--!
+
 library ieee;
 use ieee.std_logic_1164.all;
 
+--! @brief Top level module entity
 entity top is
 	port (
-		led : out std_logic;
-		sw : in std_logic
+		DDR_addr : inout std_logic_vector(15-1 downto 0);
+		DDR_ba : inout std_logic_vector(3-1 downto 0);
+		DDR_cas_n : inout std_logic;
+		DDR_ck_n : inout std_logic;
+		DDR_ck_p : inout std_logic;
+		DDR_cke : inout std_logic;
+		DDR_cs_n : inout std_logic;
+		DDR_dm : inout std_logic_vector(4-1 downto 0);
+		DDR_dq : inout std_logic_vector(32-1 downto 0);
+		DDR_dqs_n : inout std_logic_vector(4-1 downto 0);
+		DDR_dqs_p : inout std_logic_vector(4-1 downto 0);
+		DDR_odt : inout std_logic;
+		DDR_ras_n : inout std_logic;
+		DDR_reset_n : inout std_logic;
+		DDR_we_n : inout std_logic;
+		FIXED_IO_ddr_vrn : inout std_logic;
+		FIXED_IO_ddr_vrp : inout std_logic;
+		FIXED_IO_mio : inout std_logic_vector(54-1 downto 0);
+		FIXED_IO_ps_clk : inout std_logic;
+		FIXED_IO_ps_porb : inout std_logic;
+		FIXED_IO_ps_srstb : inout std_logic
 	);
 end top;
 
+--! @brief The entity has a structural architecture with
+--!        the IBD wrapper as underlying component
 architecture structural of top is
+	component bd_wrapper is
+		port (
+			DDR_addr : inout std_logic_vector(15-1 downto 0);
+			DDR_ba : inout std_logic_vector(3-1 downto 0);
+			DDR_cas_n : inout std_logic;
+			DDR_ck_n : inout std_logic;
+			DDR_ck_p : inout std_logic;
+			DDR_cke : inout std_logic;
+			DDR_cs_n : inout std_logic;
+			DDR_dm : inout std_logic_vector(4-1 downto 0);
+			DDR_dq : inout std_logic_vector(32-1 downto 0);
+			DDR_dqs_n : inout std_logic_vector(4-1 downto 0);
+			DDR_dqs_p : inout std_logic_vector(4-1 downto 0);
+			DDR_odt : inout std_logic;
+			DDR_ras_n : inout std_logic;
+			DDR_reset_n : inout std_logic;
+			DDR_we_n : inout std_logic;
+			FIXED_IO_ddr_vrn : inout std_logic;
+			FIXED_IO_ddr_vrp : inout std_logic;
+			FIXED_IO_mio : inout std_logic_vector(54-1 downto 0);
+			FIXED_IO_ps_clk : inout std_logic;
+			FIXED_IO_ps_porb : inout std_logic;
+			FIXED_IO_ps_srstb : inout std_logic
+		);
+	end component bd_wrapper;
 begin
-	led <= sw;
+	u_bd_wrapper: component bd_wrapper
+		port map (
+			DDR_addr(15-1 downto 0) => DDR_addr(14 downto 0),
+			DDR_ba(3-1 downto 0) => DDR_ba(3-1 downto 0),
+			DDR_cas_n => DDR_cas_n,
+			DDR_ck_n => DDR_ck_n,
+			DDR_ck_p => DDR_ck_p,
+			DDR_cke => DDR_cke,
+			DDR_cs_n => DDR_cs_n,
+			DDR_dm(4-1 downto 0) => DDR_dm(4-1 downto 0),
+			DDR_dq(32-1 downto 0) => DDR_dq(32-1 downto 0),
+			DDR_dqs_n(4-1 downto 0) => DDR_dqs_n(4-1 downto 0),
+			DDR_dqs_p(4-1 downto 0) => DDR_dqs_p(4-1 downto 0),
+			DDR_odt => DDR_odt,
+			DDR_ras_n => DDR_ras_n,
+			DDR_reset_n => DDR_reset_n,
+			DDR_we_n => DDR_we_n,
+			FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
+			FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
+			FIXED_IO_mio(54-1 downto 0) => FIXED_IO_mio(54-1 downto 0),
+			FIXED_IO_ps_clk => FIXED_IO_ps_clk,
+			FIXED_IO_ps_porb => FIXED_IO_ps_porb,
+			FIXED_IO_ps_srstb => FIXED_IO_ps_srstb
+		);
 end structural;
 
