7c95e54e5fe92718ecf98e59b5a45533c507a8d2
hypothermic <32428346+hypothermic@users.noreply.github.com>
2022-04-28 23:52:22 +0300

    Use Q10.22 fixed point for finer angle detail + 32 maxlines buffer

--- hls/vpu_accel.cpp
+++ hls/vpu_accel.cpp
@@ -12,16 +12,20 @@
 // include the functions from other files directly into this file
 #include "vpu_stream.cpp" // bad practice btw
 
-void _swrite(hls::stream<ap_axis<32, 1, 1, 1>> &out, float rhos[8], float thetas[8]) {
-	ap_axis<32, 1, 1, 1>		elem;
+#define HOUGH_MAXLINES	(32)
+#define FP_WIDTH	(32) //Total FP bits
+#define FP_SHIFT	(FP_WIDTH-10) // Q10.22
+
+void vpu_stream_fpwrite(hls::stream<ap_axis<32, 1, 1, 1>> &out, float rhos[HOUGH_MAXLINES], float thetas[HOUGH_MAXLINES]) {
+	ap_axis<32, 1, 1, 1>	elem;
 
 l_row:	for (int i = 0; i < 2; ++i) {
-l_col:		for (int j = 0; j < 8; ++j) {
+l_col:		for (int j = 0; j < HOUGH_MAXLINES; ++j) {
 			#pragma HLS loop_flatten off
 			#pragma HLS pipeline II=1
 
 			// pixel.last = (j == cols-1) && (i == rows-1);
-			if ((j == 8-1) && (i == 2-1)) {
+			if ((j == HOUGH_MAXLINES-1) && (i == 2-1)) {
 				elem.last = 1;
 			} else {
 				elem.last = 0;
@@ -30,9 +34,9 @@ l_col:		for (int j = 0; j < 8; ++j) {
 			elem.data = 0;
 			//elem.data(32-1, 0) = (uint32_t)((1.1f * (i*8.0f + j + (0.0000000000000000000000000000001f * rhos[j] * thetas[j]))) * (1 << 16));
 			if (i == 2-1) {
-				elem.data(32-1, 0) = (int32_t)(thetas[j] * (1 << 16));
+				elem.data(32-1, 0) = (int32_t)(thetas[j] * (1 << FP_SHIFT));
 			} else {
-				elem.data(32-1, 0) = (int32_t)(rhos[j] * (1 << 16));
+				elem.data(32-1, 0) = (int32_t)(rhos[j] * (1 << FP_SHIFT));
 			}
 			elem.keep = -1;
 			out.write(elem);
@@ -65,7 +69,7 @@ void vpu_accel_top(hls::stream<ap_axiu<24, 1, 1, 1>> &in, hls::stream<ap_axis<32
 					img_soby(in_height, in_width),
 					img_sob(in_height, in_width),
 					img_edges(in_height, in_width);
-	float				rhos[8], thetas[8];	
+	float				rhos[HOUGH_MAXLINES], thetas[HOUGH_MAXLINES];
 
 	// clang-format off
 	#pragma HLS dataflow
@@ -86,8 +90,8 @@ void vpu_accel_top(hls::stream<ap_axiu<24, 1, 1, 1>> &in, hls::stream<ap_axis<32
 	xf::cv::Sobel<XF_BORDER_CONSTANT, 3, XF_8UC1, XF_8UC1, VPU_IMAGE_MAX_HEIGHT, VPU_IMAGE_MAX_WIDTH, XF_NPPC1, false>(img_gauss, img_sobx, img_soby);
 	xf::cv::addWeighted<XF_8UC1, XF_8UC1, VPU_IMAGE_MAX_HEIGHT, VPU_IMAGE_MAX_WIDTH, XF_NPPC1>(img_sobx, 0.5, img_soby, 0.5, 0.0, img_sob);
 	xf::cv::Threshold<XF_THRESHOLD_TYPE_BINARY, XF_8UC1, VPU_IMAGE_MAX_HEIGHT, VPU_IMAGE_MAX_WIDTH, XF_NPPC1>(img_sob, img_edges, e_thres, 255);
-	xf::cv::HoughLines<1, 4, 8, 1469, 0, 180, XF_8UC1, VPU_IMAGE_MAX_HEIGHT, VPU_IMAGE_MAX_WIDTH, XF_NPPC1>(img_edges, rhos, thetas, h_thres, 8);
+	xf::cv::HoughLines<1, 4, HOUGH_MAXLINES, 1469, 0, 180, XF_8UC1, VPU_IMAGE_MAX_HEIGHT, VPU_IMAGE_MAX_WIDTH, XF_NPPC1>(img_edges, rhos, thetas, h_thres, HOUGH_MAXLINES);
 
-	_swrite(out, rhos, thetas);
+	vpu_stream_fpwrite(out, rhos, thetas);
 }
 
